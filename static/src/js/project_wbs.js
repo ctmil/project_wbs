43d21aa64b9f49a19ea86964f3bc520704f76325
morrillo <gustavo.orrillo@moldeo.coop>
2016-11-29 10:54:06 -0300

    Added rect

--- static/src/js/project_wbs.js
+++ static/src/js/project_wbs.js
@@ -1,8 +1,16 @@
-$(function () {
-	'use strict';
-	var website = openerp.website;
+//$(function () {
+odoo.define('project_wbs.project_wbs', function (require) {
+'use strict';
 
-	console.log('Hola mundo');
+var ajax = require('web.ajax');
+var config = require('web.config');
+var Widget = require('web.Widget');
+var website = require('website.website');
+
+console.log('Hola mundo');
+
+var svgContainer = d3.select("body").append("svg").attr("width", 200).attr("height", 200);
+var rectangle = svgContainer.append("rect").attr("x", 10).attr("y", 10).attr("width", 50).attr("height", 100);
 
 });
 
