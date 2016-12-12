945f7c40e340b5bf60dbd33d837551722c68c706
morrillo <gustavo.orrillo@moldeo.coop>
2016-12-12 20:16:02 -0300

    Getting closer

--- static/src/js/project_wbs.js
+++ static/src/js/project_wbs.js
@@ -23,25 +23,31 @@ Projects.query(['name'])
 
 var children = null;
 var get_children = function (project_id, project_name) {
-	if Projects.query(['name','parent_id'])
-		.filter([['parent_id', '=', project_id]]).count() > 0 {
-		Projects.query(['name','parent_id'])
-			.filter([['parent_id', '=', project_id]])
-			.all().then(function (project_data) {
-				_.each(project_data, function(project_data) {
-					console.log('Dentro #2');
-					console.log(project_data.id);
-					project_id = project_data.id;
-					project_name = project_data.name;
-					children = get_children(project_id,project_name);
+	Projects.query(['name','parent_id']).
+		filter([['parent_id', '=', project_id]]).count().then(function (item) {
+			console.log('Cantidad');
+			console.log(item);
+			if (item > 0) {
+				console.log('Esta funcionando');
+				Projects.query(['name','parent_id'])
+					.filter([['parent_id', '=', project_id]])
+					.all().then(function (project_data) {
+						_.each(project_data, function(project_data) {
+							console.log('Dentro #2');
+							console.log(project_data.id);
+							project_id = project_data.id;
+							project_name = project_data.name;
+							children = get_children(project_id,project_name);
+						}); //each
 					});
-				});
-		} else {
-			console.log('Proyecto hijo');
-			console.log(project_name);
-			return project_name;
-		}
-	};
+				} // end item > 0
+			}); // end count.then
+		//else {
+		//	console.log('Proyecto hijo');
+		//	console.log(project_name);
+		//	return project_name;
+		//}
+	}; // end function get_children
 
 console.log(get_children(project_id));
 
